863cb2e70142882081a8f363db425f4e8d59507a
128na <simutrans.128na@gmail.com>
2023-05-21 11:20:35 +0900

    セキュリティ設定

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -28,7 +28,6 @@ function createWindow() {
     height: 768,
     useContentSize: true,
     webPreferences: {
-      contextIsolation: true,
       // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
       preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
     },
